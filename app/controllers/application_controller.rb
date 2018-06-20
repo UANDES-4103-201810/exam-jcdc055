5c74f6529e499a905deb3f6c967657baf3d9349a
fmoreira <jdiaz@miuandes.cl>
2018-06-20 17:19:39 -0400

    Display pizza catalog homepage,Includes bar at the top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:lastname,:email,:password])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name,:lastname,:email,:password])
   end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Category.create(name: 'technology')
+#
+Recipe.create(name:"Pepperoni",price:500,info:"Delicioso Pepperoni")
+Recipe.create(name:"Hawaina",price:510,info:"Piña y Jamon")
+Recipe.create(name:"Pollo BBQ",price:505,info:"Pollo ,BBQ, Cebolla")
+Recipe.create(name:"Italiana",price:555,info:"Chorizo,tomate, cebolla")
+Recipe.create(name:"Mediterranea",price:5000,info:"Rucula, Jamon serrano")
+Recipe.create(name:"Ricottinni",price:500,info:"Mucha Riccootta  y jamon")
+
+Crust.create(regular:true ,price: 5000)
+Crust.create(regular:false ,price: 6000)
+
+
+
